5db97d1a47d5a3eaaa3a6dc12b2fae0252086b29
Animesh <animeshdeas@gmail.com>
2024-04-12 18:35:48 +0530

    Added Validation for Passwords

--- 1Assignment_LoginPage/main.js
+++ 1Assignment_LoginPage/main.js
@@ -1,29 +1,39 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
 
-document.getElementById("loginForm").addEventListener("submit",function(event){
-
-      event.preventDefault();
-
-      var username =document.getElementById("username").value;
-
-
-      var password =document.getElementById("password").value;
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
   
+  var alertMessages = [];
 
-      if (username.trim() === '' || username.trim().length === 0) {
-          
-        console.log("username is empty");
-    }
+  // Password validation
+  var lowerCaseLetters = /[a-z]/g;
+  var upperCaseLetters = /^[A-Z]/;
+  var numbers = /[0-9]/g;
+
+  if (!password.match(lowerCaseLetters)) {
+    alertMessages.push("Password should contain at least one lowercase letter");
+  }
+
+  if (!password.match(upperCaseLetters)) {
+    alertMessages.push("First letter of the password should be uppercase");
+  }
+
+  if (!password.match(numbers)) {
+    alertMessages.push("Password should contain at least one number");
+  }
+
+  if (password.length > 8) {
+    alertMessages.push("Password length should not exceed 8 characters");
+  }
 
 
-      if(password.trim().length===0)
-      {
-        alert("Please Enter Password");
-        return;
-      }
+  if (alertMessages.length > 0) {
+    alert(alertMessages.join("\n"));
+    return;
+  }
 
-        // If all checks pass, log the username and password to the console
-    console.log('Username:', username);
-    console.log('Password:', password);
-
-
+  // If all checks pass, log the username and password to the console
+  console.log("Username: " + username);
+  console.log("Password: " + password);
 });
