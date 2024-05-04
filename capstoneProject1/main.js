7e2bfb8a4a3171cc8100329bcd91c7fa72449142
Animesh <animeshdeas@gmail.com>
2024-05-04 13:01:22 +0530

    Implmenting features like Showing next question and restarting Quiz

--- capstoneProject1/main.js
+++ capstoneProject1/main.js
@@ -3,14 +3,27 @@ const quizPage =  document.querySelector(".quiz-page");
 const resultPage  = document.querySelector(".result-page");
 const questionDiv =  document.querySelector(".question");
 const answersDiv = document.querySelector(".answers");
-const nextButton = document.getElementsByClassName(".btn-next")
-
+const totalQuestions = document.querySelector(".total-questions")
+const progressColor = document.querySelector(".progress-colour");
+const progressTxt = document.querySelector(".progress-txt");
+const submitTotal = document.querySelector(".submit-total");
+const score  =  document.querySelector(".score");
 
 let questions =[];
 let currentIndex =0;
 let currentScore=0;
-let totalScore =0;
+let totalScore =questions.length;
+let Time =10;
+let timer;
     
+
+const progress =(value)=>{
+   
+     const percentage = (value/Time)*100;
+     progressColor.style.width = `${percentage}%`;
+     progressTxt.innerHTML = `${value}`;
+ 
+};
 
 
 
@@ -18,13 +31,24 @@ function showScore()
 {
     quizPage.classList.add("hide");
     resultPage.classList.remove("hide");
+
+    
+    const restartBtn =  document.getElementById("restart-btn");
+
+    restartBtn.addEventListener("click",()=>{
+               
+           resultPage.classList.add("hide");
+           selectingPage.classList.remove("hide");
+    });
+
 }
 
 
 function showQuiz()
 {
+    
     const currentQuestion= questions[currentIndex];
-     questionDiv.innerHTML = "";   
+    questionDiv.innerHTML = "";   
     const Question = document.createElement("p");
     Question.innerHTML = currentQuestion.question;
     questionDiv.appendChild(Question);
@@ -36,60 +60,110 @@ function showQuiz()
     })
 
     answersDiv.innerHTML ="";
-
+    
+    let answerSelected = false
+ 
+    
     allAnswers.forEach((answer)=>{
       
         const button =document.createElement("button");
         button.innerText = answer;
         button.classList.add("btn");
-        button.addEventListener("click",()=> checAnswer(answer===currentQuestion.correct_answer));
         answersDiv.appendChild(button);
+        button.addEventListener("click",()=>{
+   
+             if(!answerSelected){
+            const isCorrect = answer===currentQuestion.correct_answer;
+
+                if(isCorrect)
+                {
+                    currentScore += 1;
+                    console.log(currentScore);
+                    score.innerHTML =`
+                    <span id="score-txt">SCORE: </span>
+                    <span id="score-num">${currentScore}</span>
+                    `
+                    button.style.background = "green";
+                }
+                else 
+                {
+                    button.style.background = "red";
+                }
+            
+            }
+
+            answerSelected = true;
+          
+          
+           
+            const nxtButton =   document.getElementById("btn-next");
+            nxtButton.classList.remove("hide");
+            nxtButton.addEventListener("click",nextQuestionHandler);
+          
+            
+            const allbtn =  document.querySelectorAll(".btn");
+
+            allbtn.forEach((button)=>{
+                button.disabled = true;
+            })
+            
+
+            clearInterval(timer);
+
+          
+        })
+       
+       
     });    
+
+    totalQuestions.innerHTML =`
+    <span class="q">${currentIndex+1} </span> / 
+    <span class="total-q">${questions.length}</span>
+    `;
+    
+    startTimer(Time);
+    
+
 }
 
-
-function checAnswer(isTrue)
+function nextQuestionHandler()
 {
-    if(isTrue)
-    {
-        currentScore+=10;
-        
-    }
-    else{
-
-        if(currentIndex < questions.length)
-        {
-               
-               nextButton.addEventListener("click",()=>{
-                currentIndex++;
-                showQuiz();
-                return ;        
-
-            })
-        }
-        else{
-            showScore();
-        }
-
-    }
-
-
-    if(currentIndex < questions.length)
-    {
-        currentIndex++;
-        showQuiz();
-        
-    }
-    else{
+    currentIndex++;
+     if(currentIndex < questions.length)
+     {
        
-            nextButton.addEventListener("click",()=>{
-                
-                showScore();
-                return ;        
+        answerSelected = false;
+        showQuiz();
+     }
+     else { 
+        showScore();
+     }
+     const nxtButton = document.getElementById("btn-next");
+     nxtButton.removeEventListener("click", nextQuestionHandler);
+     nxtButton.classList.add("hide");
+}
 
-            })
+const startTimer =((time)=>{
+     
+    timer  =  setInterval(()=>{
 
-    }
+        if(timer >=0 )
+        {
+            progress(time);
+            time--;
+        }
+       
+    },1000);
+
+
+});
+
+
+
+function updateScore()
+{
+    let scoreElement = document.getElementById("score-num");
+    scoreElement.innerText = currentScore;
 }
 
 
