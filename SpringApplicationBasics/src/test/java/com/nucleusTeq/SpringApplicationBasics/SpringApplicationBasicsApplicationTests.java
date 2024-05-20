205d2cc9c3588e84b249ba08be5ea439aba78ab9
Animesh <animeshdeas@gmail.com>
2024-05-20 14:09:13 +0530

    Done with the Basics of Spring Application

---
+++ SpringApplicationBasics/src/test/java/com/nucleusTeq/SpringApplicationBasics/SpringApplicationBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nucleusTeq.SpringApplicationBasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
