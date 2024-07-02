b80ab459f5ee445efecd756220c871c84098e323
Animesh <animeshdeas@gmail.com>
2024-07-02 10:34:33 +0530

    Understanding props

--- reactPractice/Expense-tracker/src/App.jsx
+++ reactPractice/Expense-tracker/src/App.jsx
@@ -4,14 +4,25 @@ import Header from './components/Header'
 import Add from './components/Add'
 import ExpensesList from './components/ExpensesList'
 import Expenses from './components/Expenses'
+import { useState } from 'react'
 function App() {
+
+  const [showAdd, setShowAdd] = useState(false);
+  const [showExpenses, setShowExpenses]= useState(true);
+
+  const toggleAddComponent = () => {
+    setShowAdd(!showAdd);
+    setShowExpenses(!showExpenses);
+  };
+
   
 
   return (
     <>
      <div className='App'>
-         <Header/>
-         <Expenses/>
+         <Header onButtonClick={toggleAddComponent}/>
+         {showExpenses && <Expenses />}
+         {showAdd && <Add />}
          <ExpensesList/> 
       </div>  
     </>
