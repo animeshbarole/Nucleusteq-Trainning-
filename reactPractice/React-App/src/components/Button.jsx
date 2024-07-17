63edf8d0e0c91ba48e7effc0c5f12184f75890dd
Animesh <animeshdeas@gmail.com>
2024-07-17 09:00:36 +0530

    Created Basic Project Added Button

---
+++ reactPractice/React-App/src/components/Button.jsx
@@ -0,0 +1,19 @@
+
+
+
+const Button = () => {
+  
+
+    const handleClick =()=>{
+        
+        alert("Animesh is clicked the button");
+    }
+
+  return (
+    <div className="button-div">
+         <button onClick={handleClick} > Click Me</button>
+    </div>
+  )
+}
+
+export default Button
